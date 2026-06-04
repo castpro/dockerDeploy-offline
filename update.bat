9aecf8eb069a2c7a7e691ab94640c90ff5b2d6c9
castpro <castpro@castpro.co.kr>
2026-06-04 13:50:20 +0900

    fix: HDC 비활성화 및 update.bat 경로 D드라이브로 수정

--- update.bat
+++ update.bat
@@ -1,16 +1,16 @@
 @echo off
 echo === 로컬 서버 업데이트 시작 ===
 
-cd /d C:\local-server\evc_was
+cd /d D:\local-server\evc_was
 git pull origin hdc
 
-cd /d C:\local-server\ocpp
+cd /d D:\local-server\ocpp
 git pull origin offline_deploy
 
-cd /d C:\local-server\evc_webadmin
+cd /d D:\local-server\evc_webadmin
 git pull origin local-deploy
 
-cd /d C:\local-server\dockerDeploy-offline
+cd /d D:\local-server\dockerDeploy-offline
 git pull origin main
 docker-compose down
 docker-compose up -d --build
